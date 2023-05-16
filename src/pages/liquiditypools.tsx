6b7c0eb2d313bfbd83ba404172b3ddc88a742899
Generalsoi <ikhinobelesuccess@gmail.com>
2023-05-16 22:06:08 +0100

    fix: added the first page of the liquidity pools

--- src/pages/liquiditypools.tsx
+++ src/pages/liquiditypools.tsx
@@ -1,5 +1,21 @@
 import React, { FC } from "react";
 
 export const Liquiditypools: FC = () => {
-  return <div>liquiditypools</div>;
+  return (
+    <div className="flex items-center justify-center w-full h-screen font-montserrat bg-[#E5E5E5]">
+      <div className="w-full md:w-[30rem] bg-white rounded-lg p-4">
+        <div className="grid grid-cols-6">
+          <h1 className="col-span-2 font-bold text-sm">Position Overview</h1>
+          <button className="bg-[#9645D7] px-2 py-1 rounded-xl text-white text-sm col-span-2 col-end-7 flex items-center justify-center gap-2">
+            <span>+</span>
+            New Position
+          </button>
+        </div>
+
+        <button className="w-full px-2 py-4 flex items-center justify-center bg-[#52058F] text-white text-sm rounded-lg mt-6 font-bold">
+          Connect wallet
+        </button>
+      </div>
+    </div>
+  );
 };
