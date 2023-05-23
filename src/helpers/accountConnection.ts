62bd6dcaa070ee002a5a0812568841e8e1db78e2
mumeen <azeezmumeen6@gmail.com>
2023-05-23 20:05:13 +0100

    set wallet-connect to devnet

--- src/helpers/accountConnection.ts
+++ src/helpers/accountConnection.ts
@@ -5,6 +5,7 @@ import { EverscaleStandaloneClient } from "everscale-standalone-client";
 export const initVenomConnect = async () =>
   new VenomConnect({
     theme: "light",
+    checkNetworkId: 1002,
     providersOptions: {
       venomwallet: {
         links: {
@@ -26,11 +27,11 @@ export const initVenomConnect = async () =>
               fallback: () =>
                 EverscaleStandaloneClient.create({
                   connection: {
-                    id: 1000,
-                    group: "venom_testnet",
+                    id: 1002,
+                    group: "venom_devnet",
                     type: "jrpc",
                     data: {
-                      endpoint: "https://jrpc.venom.foundation/rpc",
+                      endpoint: "https://jrpc-devnet.venom.foundation/",
                     },
                   },
                 }),
