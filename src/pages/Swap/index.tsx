e5c6c194124704579ad58b4a0f202fb23dfa8914
Donice <technocrater.pdevelopers@gmail.com>
2023-05-25 11:45:14 +0100

    fixed merged conflict

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -4,7 +4,6 @@ import Navbar from "../../components/Navbar";
 export default function Swap() {
   return (
     <section className="h-screen w-screen bg-purple">
-      <Navbar />
       <SwapCard />
     </section>
   );
