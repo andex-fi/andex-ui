597e56d4db31928e00fa05dde9a79f74a07356c9
Generalsoi <ikhinobelesuccess@gmail.com>
2023-05-31 00:07:02 +0100

    fix: landing page 85 percent done

--- src/pages/homepage/hero.tsx
+++ src/pages/homepage/hero.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
+import VenomBall from "../../assets/venomball.png";
+import Cube from "../../assets/cube.png";
 
 export const Hero: FC = () => {
   return (
     <section
-      className="text-center text-white px-4 md:px-10 lg:px-20 py-10 md:pt-24"
+      className="text-center text-white px-4 md:px-10 lg:px-20 py-10 md:pt-24 pb-0"
       style={{
         background: "linear-gradient(90deg, #270A3E 0%, #100618 100%)",
       }}
@@ -40,6 +42,10 @@ export const Hero: FC = () => {
         <Button btnStyles="bg-[#3B1F5E] text-white flex items-center justify-center font-bold px-4 py-2 text-sm rounded-lg">
           Learn more
         </Button>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-0 mt-10">
+        <img src={VenomBall} alt="venomball" className="animate-bounce" />
+        <img src={Cube} alt="cube" />
       </div>
     </section>
   );
