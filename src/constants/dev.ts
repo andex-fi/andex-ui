3fe23f8e327b197ec17bc7ba26826bb6d56e64fc
Nate <89227631+natedotv2@users.noreply.github.com>
2023-05-30 18:23:06 +0100

    Added all dex contracts

--- src/constants/dev.ts
+++ src/constants/dev.ts
@@ -4,5 +4,10 @@ export const WrapGas = '1000000000' // <= 1 VENOM
 
 export const SafeAmount = '10000000000' // <= 10 VENOM
 
-export const DexRootAddress = new AddressLiteral('0:6913eb7240889bc558e1f5c556c049fa8109178e2268b66c7d741b686c507ee3')
-export const TokenFactoryAddress = new AddressLiteral('0:9daf6f5bfb814a1987eee90dfa11252baae976638f30e13f86423b7b19b2a2bb')
+export const DexRootAddress = new AddressLiteral('0:e1acd1e93ebff4892ebe37c411f524035ef5f096ff8b869a0c47d044dad34d22')
+export const TokenFactoryAddress = new AddressLiteral('0:213d1bc1e0308b76c27c0136d6e3e1307f29d3cdff97e92b82bd0ba9ad8e2ffc')
+export const VenomToTip3Address = new AddressLiteral('0:e79ececf54b024867290a764173ba85280e6fe76dc6cbaeb5a0b01ab67012afb')
+export const Tip3ToVenomAddress = new AddressLiteral('0:f4e99253c9d1b322098c2a37ac40be0f1b02bdce012da6129e6f6a75690415ba')
+export const VenomWvenomToTip3Address = new AddressLiteral('0:cfc4192b02e9684d7a6f10e9dd9c8922baf8bcd73392e7840ae20800e8d0ee1c')
+export const WVenomVaultAddress = new AddressLiteral('0:3b9739d1c977562c84399cb02cd52bc9d5aa274dd578f85e20de8f9724768e5c')
+export const WVENOMRootAddress = new AddressLiteral('0:caf849eea43d916944844ea0c4dc0bde7588ade07d81cad890dcd85bd4969ebc')
