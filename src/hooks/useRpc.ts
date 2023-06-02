bbf9ba4ea7afaab0cfaf0d587d9abbb5a2bbd460
Nate <89227631+natedotv2@users.noreply.github.com>
2023-06-02 04:59:19 +0100

    Added useRpc.ts and debug utils

---
+++ src/hooks/useRpc.ts
@@ -0,0 +1,17 @@
+import { ProviderRpcClient } from 'everscale-inpage-provider'
+
+import { debug } from '../utils/console'
+
+let rpc: ProviderRpcClient
+
+export function useRpc(): ProviderRpcClient {
+    if (rpc === undefined) {
+        debug(
+            '%cCreated a new one ProviderRpcClient instance as global connection to the VENOM Wallet',
+            'color: #8301e7',
+        )
+        rpc = new ProviderRpcClient()
+    }
+
+    return rpc
+}
