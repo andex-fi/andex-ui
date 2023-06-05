3b15ac554bdd17e5b9fd035a5e353305de302e1a
Nate <89227631+natedotv2@users.noreply.github.com>
2023-06-06 00:00:30 +0100

    FIxed connect modal

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,6 +18,8 @@ import { Link } from "react-router-dom";
 import { useAccountContext } from "../hooks/useAccountContext";
 import WalletDropDown from "./WalletDropDown";
 import { VenomConnect } from "@andex/wallet-kit";
+import { EverscaleStandaloneClient } from "everscale-standalone-client";
+import { ProviderRpcClient } from "everscale-inpage-provider";
 // import { Sun } from "@heroicons/react/20/solid";
 
 const navigation = [
@@ -36,36 +38,90 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-const themesList = ["light", "dark"]
+const standaloneFallback = () =>
+  EverscaleStandaloneClient.create({
+    connection: {
+      id: 1002,
+      group: "venom_devnet",
+      type: "jrpc",
+      data: {
+        endpoint: "https://jrpc-devnet.venom.foundation/"
+        // endpoint: "https://jrpc.venom.foundation/rpc",
+      },
+    },
+  });
+
 
 export default function Navbar() {
+  const initTheme = "light" as const;
+  const themesList = ["light", "dark"];
+  
   const { address, connect } = useAccountContext();
-  const [venomConnect] = useState<any>();
-  const [ theme, setTheme ] = useState("light");
 
   const [darkMode, setDarkMode] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [setTheme] = useState<any>(initTheme)
 
-  const getTheme = () =>
-      venomConnect?.getInfo()?.themeConfig?.name?.toString?.() || "...";
-
+  const connection = () => {
+    return new VenomConnect({
+      theme: initTheme,
+      checkNetworkId: 1002,
+      providersOptions: {
+        venomwallet: {
+          links: {},
+          walletWaysToConnect: [
+            {
+              // NPM package
+              package: ProviderRpcClient,
+              packageOptions: {
+                fallback:
+                  VenomConnect.getPromise("venomwallet", "extension") ||
+                  (() => Promise.reject()),
+                forceUseFallback: true,
+              },
+              packageOptionsStandalone: {
+                fallback: standaloneFallback,
+                forceUseFallback: true,
+              },
+  
+              // Setup
+              id: "extension",
+              type: "extension",
+            },
+          ],
+          defaultWalletWaysToConnect: [
+            // List of enabled options
+            "mobile",
+            "ios",
+            "android",
+          ],
+        },
+      },
+    });
+  };
+  
+  const getTheme = () => connection().getInfo().themeConfig.name.toString() || "...";
+  
   const onToggleThemeButtonClick = async () => {
-      const currentTheme = getTheme();
+    const currentTheme = getTheme();
+    
+    const lastIndex = themesList.length - 1;
+    
+    const currentThemeIndex = themesList.findIndex(
+      (item) => item === currentTheme
+    );
   
-      const lastIndex = themesList.length - 1;
-  
-      const currentThemeIndex = themesList.findIndex(
-        (item) => item === currentTheme
-      );
-  
-      const theme =
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const theme: any =
         currentThemeIndex >= lastIndex || !~currentThemeIndex || !~lastIndex
           ? themesList[0]
           : themesList[currentThemeIndex + 1];
   
-      await venomConnect?.updateTheme(theme);
+      await connection().updateTheme(theme);
   
       setTheme(getTheme());
-  };
+  }
+
   useEffect(() => {
     if (darkMode) {
       document.documentElement.classList.add("dark");
@@ -76,7 +132,7 @@ export default function Navbar() {
 
   const handleDarkMode = () => {
     setDarkMode(!darkMode);
-    onToggleThemeButtonClick()
+    onToggleThemeButtonClick();
   };
 
   return (
