776d287d082713acaa93348b0bcadbea2667cb09
mumeen <azeezmumeen6@gmail.com>
2023-06-05 22:55:11 +0100

    add account object to account provider

--- src/contexts/AccountProvider.tsx
+++ src/contexts/AccountProvider.tsx
@@ -8,7 +8,12 @@ import {
   useState,
 } from "react";
 import { initVenomConnect } from "../helpers/accountConnection";
-import { ProviderRpcClient } from "everscale-inpage-provider";
+import {
+  ContractState,
+  FullContractState,
+  ProviderRpcClient,
+} from "everscale-inpage-provider";
+import { Account } from "../state/WalletService";
 
 interface AccountContextType {
   connect: () => Promise<void>;
@@ -17,6 +22,9 @@ interface AccountContextType {
   venomProvider?: ProviderRpcClient;
   address?: string;
   balance?: any;
+  account?: Account;
+  contract?: ContractState | FullContractState;
+  version?: string;
 }
 
 const initialState = {
@@ -41,10 +49,11 @@ const AccountProvider = ({ children }: { children: ReactNode }) => {
   const [address, setAddress] = useState<string | undefined>();
   const [balance, setBalance] = useState<any>();
   const [standalone, setStandalone] = useState<ProviderRpcClient | undefined>();
+  const [account, setAccount] = useState<Account | undefined>();
 
-  const getAddress = async (provider: any) => {
+  const getAccount = async (provider: any) => {
     const providerState = await provider?.getProviderState?.();
-    return providerState?.permissions.accountInteraction?.address.toString();
+    return providerState?.permissions.accountInteraction;
   };
 
   const getBalance = async (provider: any, _address: string) => {
@@ -60,14 +69,16 @@ const AccountProvider = ({ children }: { children: ReactNode }) => {
   // Any interaction with venom-wallet (address fetching is included) needs to be authentificated
   const checkAuth = async (_venomConnect: any) => {
     const auth = await _venomConnect?.checkAuth();
-    if (auth) await getAddress(_venomConnect);
+    if (auth) await getAccount(_venomConnect);
   };
 
   const check = async (_provider: any) => {
-    const _address = _provider ? await getAddress(_provider) : undefined;
+    const _account = _provider ? await getAccount(_provider) : undefined;
+    const _address = _provider ? _account?.address.toString() : undefined;
     const _balance =
       _provider && _address ? await getBalance(_provider, _address) : undefined;
 
+    setAccount(_account);
     setAddress(_address);
     setBalance(_balance);
 
@@ -87,13 +98,13 @@ const AccountProvider = ({ children }: { children: ReactNode }) => {
   // By click logout. We need to reset address and balance.
   const disconnect = async () => {
     venomProvider?.disconnect();
+    setAccount(undefined);
     setAddress(undefined);
   };
 
   useEffect(() => {
     // connect event handler
     const off = venomConnect?.on("connect", onConnect);
-
     // just an empty callback, cuz we don't need it
     return () => {
       off?.();
@@ -127,8 +138,9 @@ const AccountProvider = ({ children }: { children: ReactNode }) => {
   const connect = async () => {
     try {
       await venomConnect.connect();
-      const address = await getAddress(venomConnect);
-      console.log("address: ", address);
+      const _account = await getAccount(venomConnect);
+      const address = _account?.address.toString();
+      setAccount(_account);
       setAddress(address);
     } catch (error) {
       console.log(error);
@@ -137,6 +149,7 @@ const AccountProvider = ({ children }: { children: ReactNode }) => {
   return (
     <AccountContext.Provider
       value={{
+        account,
         venomProvider,
         standalone,
         connect,
