9e1549b096ffbb7bf458918aa0caa0607f1fd88b
Nate <89227631+natedotv2@users.noreply.github.com>
2023-06-08 15:36:17 +0100

    Added state components for Launch

---
+++ src/pages/Launch/constants.ts
@@ -0,0 +1,50 @@
+import { FullContractState } from '@andex/provider'
+
+import {
+    BuilderStoreData,
+    BuilderStoreState,
+    CreateTokenStoreData,
+    CreateTokenStoreState,
+    ManageTokenStoreData,
+    ManageTokenStoreState,
+} from './types'
+
+export const DEFAULT_BUILDER_STORE_DATA: BuilderStoreData = {
+    filter: '',
+    tokens: [],
+    tokensCache: new Map<string, FullContractState>(),
+}
+
+export const DEFAULT_BUILDER_STORE_STATE: BuilderStoreState = {
+    isLoading: false,
+}
+
+export const DEFAULT_CREATE_TOKEN_STORE_DATA: CreateTokenStoreData = {
+    decimals: '',
+    name: '',
+    symbol: '',
+}
+
+export const DEFAULT_CREATE_TOKEN_STORE_STATE: CreateTokenStoreState = {
+    isCreating: false,
+}
+
+export const DEFAULT_MANAGE_TOKEN_STORE_DATA: ManageTokenStoreData = {
+    token: undefined,
+    tokenCache: undefined,
+    targetAddress: '',
+    targetWalletBalance: '',
+    amountToMint: '',
+    amountToBurn: '',
+    callbackAddress: '0:0000000000000000000000000000000000000000000000000000000000000000',
+    callbackPayload: '',
+    newOwnerAddress: '',
+}
+
+export const DEFAULT_MANAGE_TOKEN_STORE_STATE: ManageTokenStoreState = {
+    tokenRoot: '',
+    isLoading: false,
+    isMinting: false,
+    isBurning: false,
+    isTransfer: false,
+}
---
+++ src/pages/Launch/utils.ts
@@ -0,0 +1,15 @@
+import { storage } from "../../utils";
+
+export function getTokenFromLocalStorage(): string[] {
+    return JSON.parse(storage.get('builder_tokens') || '[]')
+}
+
+export function saveTokenToLocalStorage(token: string): void {
+    storage.set(
+        'builder_tokens',
+        JSON.stringify([
+            ...getTokenFromLocalStorage(),
+            token,
+        ]),
+    )
+}
