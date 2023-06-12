ddb426b92dce5af727b3c43cf8f359a9e6b7d27f
Nate <89227631+natedotv2@users.noreply.github.com>
2023-06-12 01:31:42 +0100

    fixed pool navigation error

--- src/pages/pools/hooks/useFavoritesPoolsStorage.ts
+++ src/pages/pools/hooks/useFavoritesPoolsStorage.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import * as React from "react";
 import { reaction } from "mobx";
 
@@ -37,7 +38,7 @@ export function useFavoritesPoolsStorage(): string[] {
       reaction(() => favoritePools.addresses, forceUpdate, {
         fireImmediately: true,
       }),
-    [favoritePools.addresses, forceUpdate]
+    []
   );
 
   return favoritePools.addresses ?? [];
