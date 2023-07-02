69f9ae4654d9c516d00c677540394a89c7e41443
Nate <89227631+natedotv2@users.noreply.github.com>
2023-07-02 08:32:31 +0100

    Remove deprecated classes from constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,6 +4,5 @@ import type { CommonTokenTransactionReceipt } from '../types/LiquidityTypes'
 export * from './utils'
 export * from './config'
 export * from './helpers'
-export * from './tokenWallet'
 
 export const RECEIPTS = new Map<string, CommonTokenTransactionReceipt & { poolAddress?: Address}>()
