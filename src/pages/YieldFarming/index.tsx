a01475911258e64370a093ab60667886d72fba70
Donice <technocrater.pdevelopers@gmail.com>
2023-07-03 12:07:37 +0100

    Wrapped yield farming in Page

--- src/pages/YieldFarming/index.tsx
+++ src/pages/YieldFarming/index.tsx
@@ -1,18 +1,22 @@
 import HeaderComponent from "./Header";
 import YieldFarmTableComponent from "./YieldFarmTable";
+import Page from "../../components/Layout/Page";
 
 export default function YieldFarmingPage(): JSX.Element {
   // const wallet = useWallet()
 
   return (
-    <section className="max-w-screen-xl m-auto p-8 pt-16 h-[65vh]">
+    <Page>
+      <section className="m-auto p-8 pt-16 h-[65vh]">
       <HeaderComponent />
       <YieldFarmTableComponent />
       <div className="flex flex-col items-center mt-6">
-        <button className="bg-[#52058F] text-white px-4 py-2 border rounded-full w-[10%]">
+        <button className="bg-[#52058F] text-white px-4 py-2 border rounded-full w-[40%] md:w-[10%]">
           Load more
         </button>
       </div>
     </section>
+    </Page>
+    
   );
 }
