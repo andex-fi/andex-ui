c3d14655116bcd33b1a921322067365d6d5db0b6
Donice <technocrater.pdevelopers@gmail.com>
2023-07-03 12:08:55 +0100

    Fixed button

--- src/pages/YieldFarming/index.tsx
+++ src/pages/YieldFarming/index.tsx
@@ -11,7 +11,7 @@ export default function YieldFarmingPage(): JSX.Element {
       <HeaderComponent />
       <YieldFarmTableComponent />
       <div className="flex flex-col items-center mt-6">
-        <button className="bg-[#52058F] text-white px-4 py-2 border rounded-full w-[40%] md:w-[10%]">
+        <button className="bg-[#52058F] text-white px-4 py-2 border rounded-full w-[40%] md:w-[20%]">
           Load more
         </button>
       </div>
