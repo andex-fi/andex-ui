3964f015ea8c4d5b7425da7fb1a1525f39e54461
Donice <technocrater.pdevelopers@gmail.com>
2023-07-05 22:30:28 +0100

    Fixed

--- src/pages/YieldFarming/Header.tsx
+++ src/pages/YieldFarming/Header.tsx
@@ -70,7 +70,7 @@ const HeaderComponent = () => {
 
         <select
           id="farms"
-          className="px-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-[20%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="px-4 font-bold bg-gray-50 border border-gray-300 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-[20%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
           <option defaultValue="All Farm">All Farms</option>
           <option value="US">United States</option>
